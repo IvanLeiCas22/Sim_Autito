e8243b7cd5281e9ba52ce903bcf3e97544e06f25
IvanLeiCas22 <leivacastell2002@gmail.com>
2026-05-20 14:19:05 -0300

    Definitive simulator

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Release/.qt/deploy_Sim_Autito_55c6516401.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/GAMING/Desktop/Proyectos/Sim_Autito/Sim_Autito/build/Desktop_Qt_6_11_0_MinGW_64_bit-Release/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Sim_Autito-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/GAMING/Desktop/Proyectos/Sim_Autito/Sim_Autito/build/Desktop_Qt_6_11_0_MinGW_64_bit-Release/Sim_Autito.exe"
-    GENERATE_QT_CONF
-)
